6509ce176c0f24e1610cba1233a5ed8e5da9977f
Raraykin <vand_er@mail.ru>
2023-11-05 09:52:48 +0300

    Теперь конфиг работает и все ненужные мне бинды и плагины убраны

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,9 +1,12 @@
-function ColorMyPencils(color)
+function ColorMyPencil(color)
+	-- color = color or "rose-pine"
 	color = color or "tokyonight"
 	vim.cmd.colorscheme(color)
 
+	-- Сделать фон прозрачным
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "SignColumn", { bg = "none", fg = "none" })
 end
 
-ColorMyPencils()
+ColorMyPencil()
--- lua/valery/packer.lua
+++ lua/valery/packer.lua
@@ -4,50 +4,34 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.4', -- or branch = '0.1.x'
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
 
-    --  use({
-    --	  'rose-pine/neovim',
-    --	  as = 'rose-pine',
-    --	  config = function()
-    --		  require("rose-pine").setup()
-    --		  vim.cmd('colorscheme rose-pine')
-    --	  end
-    --  })
 
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-    use('nvim-treesitter/playground')
-    use('mbbill/undotree')
-    use('tpope/vim-fugitive')
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
-        requires = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' }, -- Required
-            { 'williamboman/mason.nvim' }, -- Optional
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use('tpope/vim-fugitive')
 
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' }, -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'hrsh7th/cmp-buffer' }, -- Optional
-            { 'hrsh7th/cmp-path' }, -- Optional
-            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-            { 'hrsh7th/cmp-nvim-lua' }, -- Optional
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'}, -- Required
+			{'williamboman/mason.nvim'}, -- Optional
+			{'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-            -- Snippets
-            { 'L3MON4D3/LuaSnip' }, -- Required
-            { 'rafamadriz/friendly-snippets' }, -- Optional
-        }
-    }
-    use('folke/tokyonight.nvim')
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},     -- Required
+			{'hrsh7th/cmp-nvim-lsp'}, -- Required
+			{'L3MON4D3/LuaSnip'},     -- Required
+		}
+	}
 
+	use('rose-pine/neovim')
+	use("folke/tokyonight.nvim")
 end)
